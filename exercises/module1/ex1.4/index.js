8a22de09d7e78aa326598222c8f3c253c27f079f
Steven <stevenagbassah23@gmai.com>
2022-11-24 17:10:24 +0100

    Module 1

---
+++ exercises/module1/ex1.4/index.js
@@ -0,0 +1,14 @@
+const feuVert = document.querySelector('.green');
+const feuRouge = document.querySelector('.rouge');
+const feuOrange = document.querySelector('.orange');
+
+
+var interval;
+
+
+
+function start(){
+    interval = setInterval(feu,2000)
+}
+
+
